5923fea3390887ed36dbc1bd3f20ce135bcb9a93
kartinul <kartik.nul@gmail.com>
2026-09-12 00:19:44 +0530

    scripts for auto img.png and autogen from ./submissions/** to test

--- autogen_template_img.sh
+++ autogen_template_img.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 for d in templates/*; do
-	f=$(basename "$d")
-	c1=$(cat "$d/caption1.txt" 2>/dev/null)
-	c2=$(cat "$d/caption2.txt" 2>/dev/null)
-	c1=${c1:-_}
-	c2=${c2:-_}
-	curl -s "https://api.memegen.link/images/$f/${c1// /_}/${c2// /_}.png" -o "$d/img.png"
+	caps=""
+	for c in "$d"/caption*.txt; do caps+="/$(tr ' ' '_' <"$c")"; done
+	curl -s "https://api.memegen.link/images/$(cat "$d/meme_name.txt")$caps.png" -o "$d/img.png"
 done
